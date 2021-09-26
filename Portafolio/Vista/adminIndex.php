fbfdde68c1a8436cce199b9baf08779c4d3ca88e
FugoHaze <ssierra0770@misena.edu.co>
2021-09-26 18:10:38 -0500

    Versión 3 Portafolio,deco admin y form,otrasCosas

--- Portafolio/Vista/adminIndex.php
+++ Portafolio/Vista/adminIndex.php
@@ -4,18 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./estilos/admin.css">
+    <!-- <link rel="shortcut icon" href="./Images/mob.jpg"> -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
     <title>RUD</title>
 </head>
 <body>
     <h2>Administrador</h2>
     <h3>Aquí se mostrarán los datos que tenga almacenados en una base de datos, ademas de poder eliminarlos</h3>
-    <a href="../Vista/crudIndex.php">Registro</a>
+    <center><a role="button" class="btn" href="../Vista/crudIndex.php">Formulario de Registro</a></center>
 
     <form action="../Controlador/controladorFunciones.php" method="POST">
-        <p>Cédula:<input class="inp" type="text" name="cedula" id="cedula" placeholder="Cédula"> <br> </p>
-        <input type="submit" value="Eliminar" name="eli">
+        <p><input class="inp" type="text" name="cedula" id="cedula" placeholder="Ingresar Cédula" required> <br> </p>
+        <center><input type="submit" value="Eliminar" name="eli"></center>
     </form>
-    <table>
+    <br>
+    <center><table>
         <tr>
             <th>Cédula</th>
             <th>Nombre</th>
@@ -30,7 +34,7 @@
         $dao=new Mostrar();
 
         echo $dao->Mostrar();
-    ?></table>
+    ?></table></center>
     
 </body>
 </html>
--- Portafolio/Vista/crudIndex.php
+++ Portafolio/Vista/crudIndex.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Vista/estilos/crud.css">
+    <link rel="stylesheet" href="./estilos/form.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <title>Registro</title>
 </head>
@@ -12,19 +12,21 @@
     <h2>Formulario</h2>
     <div>
         <form action="../Controlador/controladorFunciones.php" method="POST">
-            <p>Cédula:<input class="inp" type="text" name="cedula" id="cedula" placeholder="Cédula"> <br> </p>
-            <p>Nombres:<input class="inp" type="text" name="name" id="name" placeholder="Nombres"> <br> </p>
-            <p>Apellido 1:<input class="inp" type="text" name="apellido1" id="apellido1" placeholder="Apellido 1"> <br> </p>
-            <p>Apellido 2:<input class="inp" type="text" name="apellido2" id="apellido2" placeholder="Apellido 2"> <br> </p>
-            <p>Correo:<input class="inp" type="email" name="correo" id="correo" placeholder="Correo"> <br> </p>
-            <p>Contraseña:<input class="inp" type="password" name="contra" id="contra" placeholder="Contraseña"> <br> </p>
+            <p><input class="inp" type="text" name="cedula" id="cedula" placeholder="Cédula"> <br> </p>
+            <p><input class="inp" type="text" name="name" id="name" placeholder="Nombres"> <br> </p>
+            <p><input class="inp" type="text" name="apellido1" id="apellido1" placeholder="Apellido 1"> <br> </p>
+            <p><input class="inp" type="text" name="apellido2" id="apellido2" placeholder="Apellido 2"> <br> </p>
+            <p><input class="inp" type="email" name="correo" id="correo" placeholder="Correo"> <br> </p>
+            <p><input class="inp" type="password" name="contra" id="contra" placeholder="Contraseña"> <br> </p>
 
             <br>
-            <input type="submit" value="Registrar" name="reg">
-            <input type="submit" value="Actualizar" name="act">
+            <input class="inp" type="submit" value="Registrar" name="reg">
+            <input class="inp" type="submit" value="Actualizar" name="act">
         </form>
     </div>
 
-    <a href="../Vista/adminIndex.php">Administrador</a>
+    <center><a class="btn" href="../Vista/adminIndex.php">Administrador</a></center><br>
+    <center><a class="btn" href="../Vista/index.html">Inicio</a></center>
+    <img class="gif" src="https://i.pinimg.com/originals/2b/20/bb/2b20bbb6f6f384d6a1e970d988b60047.gif" alt="">
 </body>
 </html>
