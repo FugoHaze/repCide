2adf15485c135ce88436101670c3a68b630bfccb
FugoHaze <88674207+FugoHaze@users.noreply.github.com>
2021-10-05 21:16:03 -0500

    Clase 1-10 JS Formas Instanciar objetos

---
+++ JavaScript/Clase 1-10/script.js
@@ -0,0 +1,82 @@
+//1ra Forma de instanciar objetos 
+
+var persona=new Object();
+
+persona.nombre="Juanito";
+persona.apellido="Perez";
+
+//2da Forma de instanciar objetos
+//Formato JSON
+
+var alumno={
+    nombre:'Pedrito',
+    apellido:'Clavito',
+    nombreCompleto:function(){
+        return this.nombre +" "+this.apellido;
+    }
+}
+
+console.log(alumno.nombreCompleto());
+
+var fraccion={
+    n1:1,
+    n2:2,
+    d1:2,
+    d2:2,
+    suma:function(){
+        if (this.d1==this.d2) {
+            let r1=this.n1+this.n2;
+                res=r1/this.d1;
+        }else{
+            let r1=this.n1*this.d2;
+                r2=this.n2*this.d1;
+                r3=this.d1*this.d2;
+            var s=r1+r2;
+                res=s/r3;
+        }
+        return res;
+    },
+    resta:function(){
+        if (this.d1==this.d2) {
+            let r1=this.n1-this.n2;
+                res=r1/this.d1;
+        }else{
+            let r1=this.n1*this.d2;
+                r2=this.n2*this.d1;
+                r3=this.d1*this.d2;
+            var s=r1-r2;
+                res=s/r3;
+        }
+        return res;
+    }
+}
+
+console.log(fraccion.suma());
+
+//For in
+
+for(atributo in fraccion){
+    console.log(atributo);
+}
+
+//4ta forma de instanciar objetos
+
+function Aprendiz(nombre, apellido) {
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.nombreCompleto=function(){
+        return this.nombre+" "+ this.apellido;
+    }
+}
+var ob=new Aprendiz("Luis","Diaz");
+    ob1=new Aprendiz("Carliitos","Muchito");
+console.log(ob.nombreCompleto())
+
+//5ta Forma de instanciar objetos
+
+class Estudiante{
+    constructor(nombre,apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+}
